191ec3184e696072c1cefe51f5adde84629f3f88
fattahalf <mfattahalfattika@gmail.com>
2025-02-08 19:54:12 +0700

    minor change on title and nav

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,7 +1,7 @@
 export function Navigation() {
 	return (
 		<>
-			<nav className="bg-white p-4 border border-gray-100 text-lg font-semibold mb-5 drop-shadow-lg">Share your stories</nav>
+			<nav className="bg-white p-4 border border-gray-100 text-lg font-semibold mb-5 drop-shadow-lg">Story Teller | Share us your stories</nav>
 		</>
 	);
 }
